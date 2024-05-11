1fcf611c1b3e2a9ef7ba677f22a032f6c95b0db5
amessoudi <amessoudi150@gmail.com>
2024-05-11 05:13:17 +0100

    Added two blank lines

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 from add_0 import add
 
+
 def main():
     a = 1
     b = 2
     # Ensure to use spaces instead of tabs for indentation
     print("{} + {} = {}".format(a, b, add(a, b)))
+
 
 # Two blank lines before defining another function or the execution guard
 if __name__ == "__main__":
