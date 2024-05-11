c358c980975d98d358b9b1fb8a3ac0623d55af15
amessoudi <amessoudi150@gmail.com>
2024-05-11 05:11:25 +0100

    updated chmod and PEPS validation

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,11 @@
 from add_0 import add
 
 def main():
-	a = 1
-	b = 2
-	# Use string formatting with the format() method in one print statement
-	print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    # Ensure to use spaces instead of tabs for indentation
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
+# Two blank lines before defining another function or the execution guard
 if __name__ == "__main__":
-	main()
+    main()
